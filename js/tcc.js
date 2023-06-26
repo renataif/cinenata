39ef697960e7f04d3629d6ef24624d04bb5a4ce5
Renata Alves <90872308+renataif@users.noreply.github.com>
2023-06-26 09:45:48 -0300

    testephone2

--- js/tcc.js
+++ js/tcc.js
@@ -1,4 +1,4 @@
-function mascara(i,j){
+function mascara(i,){
    
     var v = i.value;
     
@@ -6,15 +6,13 @@ function mascara(i,j){
        i.value = v.substring(0, v.length-1);
        return;
     }
-    if(j=='cpf'){
-        i.setAttribute("maxlength", "13");
-        if (v.length == 3 || v.length == 7) i.value += ".";
-        if (v.length == 11) i.value += "-";
-    }else{
-        i.setAttribute("maxlength", "15");
-        if (v.length == 1) i.value += "(";
-        if (v.length == 2) i.value += ") ";
-        if (v.length == 9) i.value += "-";
-    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
  
- }
+}
+
+$(document).ready(function(){
+    $('#phone').mask('(00) 0000-0000');
+});
