685f6e58ee1f93b137d0513d63fdbc182536782c
Renata Alves <90872308+renataif@users.noreply.github.com>
2023-06-26 09:57:51 -0300

    test5

--- js/tcc.js
+++ js/tcc.js
@@ -1,4 +1,4 @@
-function mascara(i,){
+function mascara(i,j){
    
     var v = i.value;
     
@@ -7,12 +7,24 @@ function mascara(i,){
        return;
     }
     
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
- 
+    if(j=='cpf'){
+        i.setAttribute("maxlength", "14");
+        if (v.length == 3 || v.length == 7) i.value += ".";
+        if (v.length == 11) i.value += "-";
+    } else{
+        let r = i.replace(/\D/g, "");
+        r = r.replace(/^0/, "");
+    
+        if (r.length > 11) {
+        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+        } else if (r.length > 7) {
+        r = r.replace(/^(\d\d)(\d{5})(\d{0,4}).*/, "($1) $2-$3");
+        } else if (r.length > 2) {
+        r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+        } else if (i.trim() !== "") {
+        r = r.replace(/^(\d*)/, "($1");
+        }
+        return r;
+    }
 }
 
-$(document).ready(function(){
-    $('#phone').mask('(00) 0000-0000');
-});
