083facc4833e1bf9a56bfb40a64049969e642b1a
Renata Alves <90872308+renataif@users.noreply.github.com>
2023-06-26 10:01:48 -0300

    test9

--- js/tcc.js
+++ js/tcc.js
@@ -12,19 +12,10 @@ function mascara(i,j){
         if (v.length == 3 || v.length == 7) i.value += ".";
         if (v.length == 11) i.value += "-";
     } else{
-        let r = i.replace(/\D/g, "");
-        r = r.replace(/^0/, "");
-    
-        if (r.length > 11) {
-        r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-        } else if (r.length > 7) {
-        r = r.replace(/^(\d\d)(\d{5})(\d{0,4}).*/, "($1) $2-$3");
-        } else if (r.length > 2) {
-        r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-        } else if (i.trim() !== "") {
-        r = r.replace(/^(\d*)/, "($1");
-        }
-        return r;
+        i.setAttribute("maxlength", "14");
+        if (v.length == 0) i.value += "(";
+        if (v.length == 2) i.value += ")";
+        if (v.length == 9) i.value += "-";
     }
 }
 
