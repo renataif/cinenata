69846e11fcdd51b2884b213f47b007e7347ddf93
Renata Alves <90872308+renataif@users.noreply.github.com>
2023-06-26 10:40:05 -0300

    test5

--- js/tcc.js
+++ js/tcc.js
@@ -11,11 +11,15 @@ function mascara(i,j){
         i.setAttribute("maxlength", "14");
         if (v.length == 3 || v.length == 7) i.value += ".";
         if (v.length == 11) i.value += "-";
-    } else{ 
+    } else if(j=='tel'){ 
         i.setAttribute("maxlength", "15");
         if(v.length == 1) i.value = "(" + i.value;
         if (v.length == 3) i.value += ") ";
         if (v.length == 10) i.value += "-";
+    }
+    else{
+        i.setAttribute("maxlength", "9");
+        if (v.length == 5) i.value += "-";
     }
 }
 
